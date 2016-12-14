82569baaf8d1bbf30828413d8ae9df621e144cb7
Raúl Santos <borfast@gmail.com>
2016-12-14 12:16:25 +0000

    Use ES6 arrow functions

--- src/reverser.js
+++ src/reverser.js
@@ -1,13 +1,14 @@
 import {ALPHANUM} from './constants';
 
+// This gives us the result of list5() from data.js
 export function reverse_indexers(password) {
-    return password.split().map(function (e) {
+    return password.split().map(e => {
         return ALPHANUM.indexOf(e);
     });
 }
 
 export function reverse_modulus36(indexers) {
-    return indexers.map(function (e) {
+    return indexers.map(e => {
         if (e >= 12 && e <= 35) {
             return e + 36;
         } else if (e >= 0 && e <= 18) {
